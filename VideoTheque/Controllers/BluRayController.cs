71f273740de04d82fe64c86c4d94fef779836d53
Victor VENULETH <victor.venuleth@reseau.eseo.fr>
2024-12-16 08:56:01 +0100

    Test with repositories

--- VideoTheque/Controllers/BluRayController.cs
+++ VideoTheque/Controllers/BluRayController.cs
@@ -6,6 +6,8 @@ using VideoTheque.Businesses.Genres;
 using VideoTheque.Businesses.Personne;
 using VideoTheque.DTOs;
 using VideoTheque.Repositories.AgeRating;
+using VideoTheque.Repositories.Genres;
+using VideoTheque.Repositories.PersonneRepository;
 using VideoTheque.ViewModels;
 
 namespace VideoTheque.Controllers
@@ -15,19 +17,19 @@ namespace VideoTheque.Controllers
     public class BluRayController
     {
         private readonly IBluRayBusiness _bluRayBusiness;
-        private readonly IPersonneBusiness _personneBusiness;
-        private readonly IAgeRatingBusiness _ageRatingBusiness;
-        private readonly IGenresBusiness _genresBusiness;
+        private readonly IPersonneRepository _personneRepository;
+        private readonly IAgeRatingRepository _ageRatingRepository;
+        private readonly IGenresRepository _genresRepository;
         protected readonly ILogger<BluRayController> _logger;
 
         public BluRayController(ILogger<BluRayController> logger, IBluRayBusiness bluRayBusiness,
-            IPersonneBusiness personneBusiness, IAgeRatingBusiness ageRatingBusiness, IGenresBusiness genresBusiness)
+            IPersonneRepository personneRepository, IAgeRatingRepository ageRatingRepository, IGenresRepository genresRepository)
         {
             _logger = logger;
             _bluRayBusiness = bluRayBusiness;
-            _personneBusiness = personneBusiness;
-            _ageRatingBusiness = ageRatingBusiness;
-            _genresBusiness = genresBusiness;
+            _personneRepository = personneRepository;
+            _ageRatingRepository = ageRatingRepository;
+            _genresRepository = genresRepository;
         }
 
         [HttpGet]
@@ -53,7 +55,7 @@ namespace VideoTheque.Controllers
                 var directorNames = filmVm.Director.Split(" ");
                 if (directorNames.Length < 2)
                     return Results.BadRequest("Director's full name is required.");
-                var director = _personneBusiness.GetPersonne(directorNames[0], directorNames[1])
+                var director = _personneRepository.GetPersonne(directorNames[0], directorNames[1])
                     ?.Adapt<PersonneViewModel>();
                 if (director == null)
                     return Results.NotFound("Director not found.");
@@ -61,22 +63,22 @@ namespace VideoTheque.Controllers
                 var writerNames = filmVm.Writer.Split(" ");
                 if (writerNames.Length < 2)
                     return Results.BadRequest("Writer's full name is required.");
-                var writer = _personneBusiness.GetPersonne(writerNames[0], writerNames[1])?.Adapt<PersonneViewModel>();
+                var writer = _personneRepository.GetPersonne(writerNames[0], writerNames[1])?.Adapt<PersonneViewModel>();
                 if (writer == null)
                     return Results.NotFound("Writer not found.");
                 
                 var actorNames = filmVm.MainActor.Split(" ");
                 if (actorNames.Length < 2)
                     return Results.BadRequest("Main actor's full name is required.");
-                var mainActor = _personneBusiness.GetPersonne(actorNames[0], actorNames[1])?.Adapt<PersonneViewModel>();
+                var mainActor = _personneRepository.GetPersonne(actorNames[0], actorNames[1])?.Adapt<PersonneViewModel>();
                 if (mainActor == null)
                     return Results.NotFound("Main actor not found.");
                 
-                var ageRating = _ageRatingBusiness.GetAgeRating(filmVm.AgeRating);
+                var ageRating = _ageRatingRepository.GetAgeRating(filmVm.AgeRating);
                 if (ageRating == null)
                     return Results.NotFound("Age rating not found.");
 
-                var genre = _genresBusiness.GetGenre(filmVm.Genre);
+                var genre = _genresRepository.GetGenre(filmVm.Genre);
                 if (genre == null)
                     return Results.NotFound("Genre not found.");
                 
@@ -109,7 +111,7 @@ namespace VideoTheque.Controllers
             var directorNames = filmVm.Director.Split(" ");
                 if (directorNames.Length < 2)
                     return Results.BadRequest("Director's full name is required.");
-                var director = _personneBusiness.GetPersonne(directorNames[0], directorNames[1])
+                var director = _personneRepository.GetPersonne(directorNames[0], directorNames[1])
                     ?.Adapt<PersonneViewModel>();
                 if (director == null)
                     return Results.NotFound("Director not found.");
@@ -117,22 +119,22 @@ namespace VideoTheque.Controllers
                 var writerNames = filmVm.Writer.Split(" ");
                 if (writerNames.Length < 2)
                     return Results.BadRequest("Writer's full name is required.");
-                var writer = _personneBusiness.GetPersonne(writerNames[0], writerNames[1])?.Adapt<PersonneViewModel>();
+                var writer = _personneRepository.GetPersonne(writerNames[0], writerNames[1])?.Adapt<PersonneViewModel>();
                 if (writer == null)
                     return Results.NotFound("Writer not found.");
                 
                 var actorNames = filmVm.MainActor.Split(" ");
                 if (actorNames.Length < 2)
                     return Results.BadRequest("Main actor's full name is required.");
-                var mainActor = _personneBusiness.GetPersonne(actorNames[0], actorNames[1])?.Adapt<PersonneViewModel>();
+                var mainActor = _personneRepository.GetPersonne(actorNames[0], actorNames[1])?.Adapt<PersonneViewModel>();
                 if (mainActor == null)
                     return Results.NotFound("Main actor not found.");
                 
-                var ageRating = _ageRatingBusiness.GetAgeRating(filmVm.AgeRating);
+                var ageRating = _ageRatingRepository.GetAgeRating(filmVm.AgeRating);
                 if (ageRating == null)
                     return Results.NotFound("Age rating not found.");
 
-                var genre = _genresBusiness.GetGenre(filmVm.Genre);
+                var genre = _genresRepository.GetGenre(filmVm.Genre);
                 if (genre == null)
                     return Results.NotFound("Genre not found.");
                 
